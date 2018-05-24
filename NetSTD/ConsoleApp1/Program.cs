f736544138b3a1456b15093157447b5d8f19c20c
Cornel <cornel.hattingh@archsoftware.co.za>
2018-05-24 09:57:47 +0200

    Commit Project

---
+++ NetSTD/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using NetSTD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo();
+        }
+
+        private static void Foo()
+        {
+            var class1 = new Class1();
+            class1.Foo(System.IO.Ports.Parity.Even);
+        }        
+    }
+}
---
+++ NetSTD/NetSTD/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NetSTD
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+        public void Foo(System.IO.Ports.Parity p)
+        {
+            
+        }
+    }
+}
